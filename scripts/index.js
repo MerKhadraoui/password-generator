a606b8b4a121920064042b9d7f975ce9022f4b55
meriemkhadraoui <miyakhad@gmail.com>
2021-04-29 14:44:29 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,82 +1,72 @@
-// const submit = () => {
-//   let useremail = document.querySelector("#emailUser").value;
-//   let userPass = document.querySelector("#passUser").value;
-//   // Checking my input if it has a text
-//   if (useremail != "" && userPass.length > "8") {
-//     let newLi = document.createElement("li");
-//     let newNav = document.createElement("nav");
-//     let text = document.createTextNode(useremail);
-//     let pass = document.createTextNode(userPass);
-//     newLi.appendChild(text);
-//     newLi.appendChild(newNav);
-//     newNav.appendChild(pass);
-//     document.querySelector(".result").appendChild(newLi);
-//     document.querySelector("#passUser").value = "";
-//     document.querySelector("#emailUser").value = "";
-//   } else {
-//     document.querySelector(
-//       "#emailUser"
-//     ).placeholder = ` Please Enter valid Email`;
-//     document.querySelector("#passUser").placeholder =
-//       "Please Enter valid password";
-//   }
-// };
-
-const submit = () => {
-  let numbers = [0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
-  let alpha = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  let charactersSp = ["!", "§", "$", "%", "&", "?", "@", "€"];
-  const passGen = () => {
-    let result = "";
-    const randomNumGetter = (arr) => {
-      return Math.floor(Math.random() * arr.length);
-    };
-    for (let i = 0; i <= 17; i++) {
-      if (i % 2 == 0) {
-        result += numbers[randomNumGetter(numbers)];
-      }
-      // some other if
-      if (i % 3 == 0) {
-        result += alpha[randomNumGetter(alpha)].toUpperCase();
-      } else {
-        result += alpha[randomNumGetter(alpha)];
-      }
-      // some other if
-      if (i % 5 == 0) {
-        result += charactersSp[randomNumGetter(charactersSp)];
-      }
-    }
-    return result;
+let numbers = [0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
+let alpha = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let charactersSp = ["!", "§", "$", "%", "&", "?", "@", "€"];
+const passGen = () => {
+  //   let numbArr = document.querySelector("#numb");
+  //   let alphArr = document.querySelector("#alph");
+  //   let charArr = document.querySelector("#chars");
+  let result = "";
+  const randomNumGetter = (arr) => {
+    return Math.floor(Math.random() * arr.length);
   };
-  console.log(passGen());
-  let numbArr = document.querySelector("#numb");
-  let alphArr = document.querySelector("#alph");
-  let charArr = document.querySelector("#chars");
-  //document.querySelector('#accept:checked') !== null
+  const passLength = document.querySelector("#input2").value;
+  // document.querySelector(
+  //   "#input2"
+  // ).placeholder = `The Pass Length should be divisible by 3`;
+  let leng = Math.floor(passLength * 3);
+  for (let i = 0; i < leng; i++) {
+    if (document.querySelector("#numb:checked") !== null) {
+      result += numbers[randomNumGetter(numbers)];
+    }
+    // some other if
+    if (document.querySelector("#alph:checked") !== null) {
+      result += alpha[randomNumGetter(alpha)].toUpperCase();
+    }
+    // some other if
+    if (document.querySelector("#chars:checked") !== null) {
+      result += charactersSp[randomNumGetter(charactersSp)];
+    }
+  }
+
+  return result;
+};
+console.log(passGen());
+const btn = document.querySelector(".btn");
+const divText = document.querySelector(".print");
+btn.onclick = () => {
+  if (
+    document.querySelector("#numb:checked") == null &&
+    document.querySelector("#alph:checked") == null &&
+    document.querySelector("#chars:checked") == null
+  ) {
+    alert(" please, selct the Chars possibles");
+  } else {
+    divText.innerHTML += `${passGen()}  <br>`;
+  }
 };
